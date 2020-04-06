9b65861d9699a8d436f4098269aacfa3fc8fad70
Fahir Mumdžić <fahir.mumdzic@edu.fit.ba>
2020-04-06 20:13:43 +0200

    Commit bug fixed

--- HealthCare020.Core/Request/TokenPosetaUpsertRequest.cs
+++ HealthCare020.Core/Request/TokenPosetaUpsertRequest.cs
@@ -4,7 +4,6 @@ namespace HealthCare020.Core.Request
 {
     public class TokenPosetaUpsertRequest
     {
-        [Required(ErrorMessage = Resources)]
         [StringLength(maximumLength: 9, MinimumLength = 5)]
         [DataType(DataType.Text)]
         public string Value { get; set; }
