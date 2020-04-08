5864481204cee784f2c195f42ea0249a3de2dc93
Fahir Mumdžić <fahir.mumdzic@edu.fit.ba>
2020-04-09 01:14:01 +0200

    Implemented LicniPodaci service and controller.

---
+++ HealthCare020.Core/Request/LicniPodaciUpsertRequest.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthCare020.Core.Request
+{
+    public class LicniPodaciUpsertRequest
+    {
+        [Required(ErrorMessage = "Obavezno polje", AllowEmptyStrings = false)]
+        [StringLength(maximumLength: 15, MinimumLength = 2)]
+        public string Ime { get; set; }
+
+        [Required(ErrorMessage = "Obavezno polje", AllowEmptyStrings = false)]
+        [StringLength(maximumLength: 15, MinimumLength = 2)]
+        public string Prezime { get; set; }
+
+        [Required(ErrorMessage = "Obavezno polje", AllowEmptyStrings = false)]
+        [StringLength(maximumLength: 30, MinimumLength = 3)]
+        public string Adresa { get; set; }
+
+        [Required(ErrorMessage = "Obavezno polje", AllowEmptyStrings = false)]
+        [DefaultValue("M")]
+        public char Pol { get; set; }
+
+        [Required(ErrorMessage = "Obavezno polje", AllowEmptyStrings = false)]
+        [DataType(DataType.PhoneNumber)]
+        public string BrojTelefona { get; set; }
+
+        [Required(ErrorMessage = "Obavezno polje", AllowEmptyStrings = false)]
+        public int GradId { get; set; }
+    }
+}
