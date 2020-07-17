586e7c90d632fe97df4af86f9089550d6fbc1e9a
fahirmdz <fahir@live.com>
2020-07-18 00:17:41 +0200

    Basic design of ZahteviZaPregled stranice, using Expanders (with experimental flag)

---
+++ Healthcare020.Mobile/Healthcare020.Mobile.Android/Effects/FocusEffect.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Healthcare020.Mobile.Droid.Effects;
+using Java.Util;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportEffect(typeof(FocusEffect), nameof(FocusEffect))]
+namespace Healthcare020.Mobile.Droid.Effects
+{
+    public class FocusEffect : PlatformEffect
+    {
+        private float scaleX;
+        private float scaleY;
+
+
+        protected override void OnAttached()
+        {
+            try
+            {
+                scaleX = Control.ScaleX;
+                scaleY = Control.ScaleY;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+
+        protected override void OnElementPropertyChanged(System.ComponentModel.PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
+            try
+            {
+                if (args.PropertyName == "IsFocused")
+                {
+                    if (Control.ScaleY-scaleY>1)
+                    {
+                        Control.ScaleY = scaleY;
+                        Control.ScaleX = scaleX;
+                    }
+                    else
+                    {
+                        Control.ScaleY = scaleY + 5;
+                        Control.ScaleX = scaleX + 5;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+    }
+}
--- Healthcare020.Mobile/Healthcare020.Mobile/Constants/ResourceKeys.cs
+++ Healthcare020.Mobile/Healthcare020.Mobile/Constants/ResourceKeys.cs
@@ -6,6 +6,7 @@ namespace Healthcare020.Mobile.Constants
     {
         public static string CustomBlueColor { get; } = "CustomBlue";
         public static string HealthcareCyanColor { get; } = "HealthcareCyan";
+        public static string CustomNavyBlueDarkColor { get; } = "CustomNavyBlueDark";
         public static string LightSmokeWhiteColor { get; } = "LightSmokeWhite";
         public static string CustomDarkBlueColor { get; } = "CustomDarkBlue";
         public static string CustomDarkCyanColor { get; } = "CustomDarkCyan";
@@ -13,5 +14,6 @@ namespace Healthcare020.Mobile.Constants
         public static string TitlePathLarge { get; } = "TitlePath-Large";
         public static string TitleLabelMedium { get; } = "TitleLabel-Medium";
         public static string TitleLabelLarge { get; } = "TitleLabel-Large";
+
     }
 }
